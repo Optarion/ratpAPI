24049d321db84ac51577cac00aa28088e0a5ab4c
Adrien Rousselet <adrien@cogitstudio.com>
2017-02-28 15:36:22 +0100

    @0.5h : UPDATE layout/script/styles- WIP

--- js/init.js
+++ js/init.js
@@ -8,43 +8,44 @@ function connectionError(message) {
 connectionError.prototype = Object.create(Error.prototype);
 connectionError.prototype.constructor = connectionError;
 
-try{
-	function loadXMLDoc(url) {
-		var xmlhttp = new XMLHttpRequest();
+	try{
+		function loadXMLDoc(url) {
+			var xmlhttp = new XMLHttpRequest();
 
-		xmlhttp.onreadystatechange = function() {
-			if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
-				if (xmlhttp.status == 200) {
-					var data = JSON.parse(xmlhttp.response),
-						response = data.response,
-						responseInfo = response.informations,
-						lineId = responseInfo.line,
-						lineType = capitalizeWords(responseInfo.type),
-						station = responseInfo.station.name,
-						schedules = response.schedules,
-						schedulesArray = Object.keys(schedules).map(function(k) { return schedules[k]; });
+			xmlhttp.onreadystatechange = function() {
+				if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+					if (xmlhttp.status == 200) {
+						var data = JSON.parse(xmlhttp.response),
+							response = data.response,
+							responseInfo = response.informations,
+							lineId = responseInfo.line,
+							lineType = capitalizeWords(responseInfo.type), //TODO : Find why function doesn't work
+							station = responseInfo.station.name,
+							schedules = response.schedules,
+							schedulesArray = Object.keys(schedules).map(function(k) { return schedules[k]; });
+						
+						//ADD {line}/{station} as breadcrumb
+						document.querySelector(".breadcrumb-station").innerHTML = station;
+						document.querySelector(".breadcrumb-busline").innerHTML = lineType + ' ' + lineId;
 
-console.log('response', response)
-					
-					//ADD {line}/{station} as breadcrumb
-					document.getElementById("breadcrumb").innerHTML = station + ' / ' + lineType + ' ' + lineId;
-
-					document.getElementById("nextSchedule").innerHTML = schedulesArray[0]["message"];
-					document.getElementById("otherSchedule").innerHTML = schedulesArray[1]["message"];
+						document.getElementById("nextSchedule").innerHTML = schedulesArray[0]["message"];
+						document.getElementById("otherSchedule").innerHTML += lineType + " in " + schedulesArray[1]["message"];
+					}
+					else {
+						throw new connectionError("The API server seems offline or you made a bad request");
+						document.getElementByClassName("error").innerHTML = 'Un problème technique empèche d\'obtenir les horaires désirés';
+					}
 				}
-				else {
-					throw new connectionError("The API server seems offline or you made a bad request");
-					document.getElementByClassName("error").innerHTML = 'Un problème technique empèche d\'obtenir les horaires désirés';
-				}
-			}
-		};
+			};
 
-		xmlhttp.open("GET", url, true);
-		xmlhttp.send();
-	}
+			xmlhttp.open("GET", url, true);
+			xmlhttp.send();
+		}
 
 	//Default display
 	loadXMLDoc('https://api-ratp.pierre-grimaud.fr/v2/bus/83/stations/4008?destination=248');
+
+	//TODO: make function to construct url
 	} catch (e) {
 		console.error(e);
 	}
